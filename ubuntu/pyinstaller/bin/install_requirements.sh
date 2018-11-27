4336688866e95569b8a6635bd698df3b86c8d748
toporkov.michael <toporkov.michael@gmail.com>
2018-11-27 13:26:11 +0300

    Add ubuntu/python and ubuntu/pyinstaller images

---
+++ ubuntu/pyinstaller/bin/install_requirements.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ -f requirements.txt ]; then
+    echo "requirements.txt file found; Installing dependencies with pip"
+    pip install --no-cache-dir --disable-pip-version-check -r requirements.txt
+elif [ -f setup.py ]; then
+    echo "setup.py file found; Installing dependencies with pip"
+    pip install --no-cache-dir --disable-pip-version-check .
+elif [ -f poetry.lock ]; then
+    echo "poetry.lock file found; Installing dependencies using poetry"
+    source $HOME/.poetry/env && poetry install --no-dev --no-interaction
+elif [ -f pyproject.toml ]; then
+    echo "pyproject.toml file found; Trying to install dependencies using poetry"
+    source $HOME/.poetry/env && poetry install --no-dev --no-interaction
+else
+    echo "No file specifying requirements found; No dependencies are installed"
+fi
+
+
+echo "Installing PyInstaller & pycrypto"
+git clone --depth 1 --single-branch --branch=v3.4 https://github.com/pyinstaller/pyinstaller.git /tmp/pyinstaller \
+    && cd /tmp/pyinstaller/bootloader \
+    && python ./waf configure --no-lsb all \
+    && pip install .. \
+    && cd ~ \
+    && rm -Rf /tmp/pyinstaller \
+    && pip install --no-cache-dir --disable-pip-version-check pycrypto
