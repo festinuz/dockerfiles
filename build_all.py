669ac4f9ceadf63432ce8716dca70121546a6f2c
toporkov.michael <toporkov.michael@gmail.com>
2018-11-27 13:40:36 +0300

    Update build-all config

--- build_all.py
+++ build_all.py
@@ -17,17 +17,31 @@ class ImageData:
 image_datas = list()
 # Add ubuntu images
 for version in ["16.04"]:
-    base = ImageData(path="ubuntu/base", name=f"ubuntu:{version}", VERSION=version)
+    base = ImageData(
+        path="ubuntu/base", name=f"ubuntu:{version}", build_args=dict(VERSION=version)
+    )
     chromedriver = ImageData(
         path="ubuntu/chromedriver",
         name=f"ubuntu:{version}-chromedriver",
-        PREFIX=NAME_PREFIX,
-        VERSION=version,
+        build_args=dict(PREFIX=NAME_PREFIX, VERSION=version),
     )
-    image_datas.extend([base, chromedriver])
+    python_version = "3.6.6"
+    python = ImageData(
+        path="ubuntu/python",
+        name=f"ubuntu:{version}-python{python_version}",
+        build_args=dict(UBUNTU_VERSION=version),
+    )
+    pyinstaller = ImageData(
+        path="ubuntu/pyinstaller",
+        name=f"ubuntu:{version}-pyinstaller{python_version}",
+        build_args=dict(
+            PREFIX=NAME_PREFIX, UBUNTU_VERSION=version, PYTHON_VERSION=python_version
+        ),
+    )
+    image_datas.extend([base, chromedriver, python, pyinstaller])
 
 
 for image_data in image_datas:
     print(f"Building: {image_data}")
     full_image_name = NAME_PREFIX + image_data.name
-    build_docker_image(image_data.path, full_image_name)
+    build_docker_image(image_data.path, full_image_name, image_data.build_args)
